646ddab70216ba1ae86a6d9edacbd72e4a73913e
IamChetang <96056914+IamPoojaG@users.noreply.github.com>
2023-01-19 20:25:51 +0530

    changed context page

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -31,8 +31,8 @@ const initialState = {
   expenses: [
     { id: uuidv4(), name: 'Shopping', cost: 50 },
     { id: uuidv4(), name: 'Holiday', cost: 300 },
-    { id: uuidv4(), name: 'Transportation', cost: 70 },
-    { id: uuidv4(), name: 'Fuel', cost: 40 },
+    { id: uuidv4(), name: 'Travel', cost: 70 },
+    { id: uuidv4(), name: 'Diesel', cost: 40 },
     { id: uuidv4(), name: 'Child Care', cost: 500 },
   ],
 };
